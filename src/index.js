17edebd198d61d624ce0ec8e518d3d98814c9df6
Viktar Svintsitski <jetalai3@gmail.com>
2019-09-30 22:51:21 +0300

    feat: add morse decoding

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const morseWords = [];
+    let i = 0;
+        while (i != expr.length) {
+            morseWords.push(getMorseChar(expr.slice(i, i+10)));
+            i += 10;
+        }
+    return  morseWords.map(item => (item === '**********' ? ' ' : MORSE_TABLE[item])).join('');
+}
+
+function getMorseChar(expression) {
+    let res = [];
+    let i = 0;
+    while (i != expression.length) {
+        if (expression.slice(i, i + 2) === '00') {
+            res.push('');
+        } else if (expression.slice(i, i + 2) === '10') {
+            res.push('.');
+        } else if (expression.slice(i, i + 2) === '11') {
+            res.push('-');
+        } else if (expression.slice(i, i + 2) === '**') {
+            res.push('**');
+        }
+        i += 2;
+    }
+    return res.join('');  
 }
 
 module.exports = {
